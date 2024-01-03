9edee0e4d03cc31fa1a9ada9dc75e27471b8785f
Josep Barberà Reverté <99675044+JosepBarbera@users.noreply.github.com>
2024-01-03 16:36:33 +0100

    Update analysis.py
    Ia funcionen los testos

--- Reinforcement/analysis.py
+++ Reinforcement/analysis.py
@@ -19,56 +19,44 @@
 # Set the given parameters to obtain the specified policies through
 # value iteration.
 
-def question2():
-    answerDiscount = 0.9
-    answerNoise = 0
-    return answerDiscount, answerNoise
+def question2a():
+  answerDiscount = 0.1
+  answerNoise = 0
+  answerLivingReward = -1
+  return answerDiscount, answerNoise, answerLivingReward
+  # If not possible, return 'NOT POSSIBLE'
 
-def question3a():
-    answerDiscount = 0.1
-    answerNoise = 0
-    answerLivingReward = -1
-    return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
+def question2b():
+  answerDiscount = 0.2
+  answerNoise = 0.1
+  answerLivingReward = 0.4
+  return answerDiscount, answerNoise, answerLivingReward
+  # If not possible, return 'NOT POSSIBLE'
 
-def question3b():
-    answerDiscount = 0.2
-    answerNoise = 0.1
-    answerLivingReward = 0.4
-    return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
+def question2c():
+  answerDiscount = 0.9
+  answerNoise = 0
+  answerLivingReward = 0
+  return answerDiscount, answerNoise, answerLivingReward
+  # If not possible, return 'NOT POSSIBLE'
 
-def question3c():
-    answerDiscount = 1
-    answerNoise = 0
-    answerLivingReward = -1
-    return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
+def question2d():
+  answerDiscount = 0.9
+  answerNoise = 0.2
+  answerLivingReward = 0
+  return answerDiscount, answerNoise, answerLivingReward
+  # If not possible, return 'NOT POSSIBLE'
 
-def question3d():
-    answerDiscount = 0.4
-    answerNoise = 0.2
-    answerLivingReward = 5
-    return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
-
-def question3e():
-    answerDiscount = 0.1
-    answerNoise = 0
-    answerLivingReward = 10
-    return answerDiscount, answerNoise, answerLivingReward
-    # If not possible, return 'NOT POSSIBLE'
-
-def question8():
-    answerEpsilon = None
-    answerLearningRate = None
-    #return answerEpsilon, answerLearningRate
-    return 'NOT POSSIBLE'
+def question2e():
+  answerDiscount = 0.1
+  answerNoise = 0
+  answerLivingReward = 10
+  return answerDiscount, answerNoise, answerLivingReward
     # If not possible, return 'NOT POSSIBLE'
 
 if __name__ == '__main__':
-    print('Answers to analysis questions:')
-    import analysis
-    for q in [q for q in dir(analysis) if q.startswith('question')]:
-        response = getattr(analysis, q)()
-        print('  Question %s:\t%s' % (q, str(response)))
+  print ('Answers to analysis questions:')
+  import analysis
+  for q in [q for q in dir(analysis) if q.startswith('question')]:
+    response = getattr(analysis, q)()
+    print ('  Question %s:\t%s' % (q, str(response)))
